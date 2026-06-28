f13bcf469c11d90b8689eb736c3ee639735ff2d6
dy <959958251@qq.com>
2026-06-28 16:58:22 +0800

    feat: 优化上帝模式交互，修复前端渲染细节

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import asyncio
 import random
 import string
 import time
@@ -16,7 +15,9 @@ ALLOWED_ORIGINS = [
     "https://www.minesweeper-game.site",
     "https://minesweeper-game.vercel.app",
     "http://localhost:5173",
+    "http://localhost:5174",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
 ]
 
 app.add_middleware(
